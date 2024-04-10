44b240e33e81039134749526bef9cb8623cbc4e0
SHAH <sagar.h.shah@accenture.com>
2024-04-10 14:44:07 +0530

    Added new folder and division tf file

---
+++ credentials.tf
@@ -0,0 +1,6 @@
+/*provider "genesyscloud" {
+  oauthclient_id     = "6dd71504-05a6-422f-af05-0c407c4cb885"
+  oauthclient_secret = "lrX89ZcvkHV9xDSZDLGh2bwqsr3jBkc2f0yfxJ7qMtc"
+  aws_region         = "us-west-2"
+}
+*/
---
+++ modules/division/auth_division.tf
@@ -0,0 +1,4 @@
+resource "genesyscloud_auth_division" "CICD_HIG_Demo" {
+  name        = "CICD_HIG_Demo"
+  description = "Custom Division for HIG CICD Demo"
+}
